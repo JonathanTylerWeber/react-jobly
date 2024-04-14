a162e5768c54d93700e0643e824a55796e0f7329
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-04-14 13:33:19 +0800

    display companies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import JoblyApi from './api.js'
 import './App.css';
+import CompanyList from './CompanyList.js'
+import Home from "./Home";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    async function getInfo() {
+      let companies = await JoblyApi.getCompanies();
+      setCompanies(companies);
+      setIsLoading(false);
+    }
+    getInfo();
+  }, []);
+
+  console.log(companies)
+
+  if (isLoading) {
+    return <p>Loading &hellip;</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact path="/companies"
+              element={<CompanyList data={companies} />}
+            />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div >
   );
 }
 
